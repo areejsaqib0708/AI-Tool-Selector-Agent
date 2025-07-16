ddaad77a4af36ba6da756dd0548b373599c7ffbe
areejsaqib0708 <127478330+areejsaqib0708@users.noreply.github.com>
2025-07-16 11:52:05 +0500

    Update API.py

--- API.py
+++ API.py
@@ -1,7 +1,3 @@
 def api():
-    api="AIzaSyDTIdWNPY8z4uC3UY2-Y9beTetqLDfM07k"
+    api="YOUR API HERE "  # YOUR GEMINI API HERE 
     return api
-#AIzaSyD5GbUJUFnYS-CMi2bDsSOt6R9GBE51Uv8
-#AIzaSyDbj4F2LwL1GscSMkqHzSrF9KzcWahinUM   Qasim
-#AIzaSyDTIdWNPY8z4uC3UY2-Y9beTetqLDfM07k
-#AIzaSyC2WW7ZxiB_lH3E4OKQl7cnCBUqLHlkIuc    Javaria
